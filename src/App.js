34bcfd0ab2b16a26e1e110a6677295cab8b541ed
mohankumarkodi <mohankumarkodi00@gmail.com>
2023-02-14 12:26:59 +0530

    ylytic-assign

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class CommentTable extends Component{
+  state = {comments:[],inputText:""}
+  
+
+
+componentDidMount(){
+  this.getComments()
+}
+getComments = async () =>{
+
+  const apiUrl = "https://dev.ylytic.com/ylytic/test"
+  const options = {
+    method : "GET"
+  }
+  const response = await fetch(apiUrl, options)
+    if (response.ok) {
+      const fetchedData = await response.json()
+      const updatedData = fetchedData.comments.map(comment => ({
+        at: comment.at,
+        author: comment.author,
+        like : comment.like,
+        reply: comment.reply,
+        text: comment.text,
+      }))
+      this.setState({
+        comments: updatedData,
+      
+      })
+    }
+
 }
 
-export default App;
+onchangeComment = event =>{
+  const inputText = event.target.value
+  this.setState({inputText:inputText})
+  const {comments} = this.state
+  const filteredcomments = comments.filter(comment =>comment.text.includes(inputText));
+  this.setState({comments:filteredcomments})
+}
+
+keyDownHandler = (event) => {
+  if (event.key === "Backspace") {
+    this.getComments()
+    this.onchangeComment(event)
+    
+  }
+}
+onChangeAuthor = event =>{
+  const inputAuthor = event.target.value
+  const {comments} = this.state
+  const filteredcomments = comments.filter(comment =>comment.text.includes(inputAuthor));
+  this.setState({comments:filteredcomments})
+}
+render(){
+  
+  const {comments} = this.state
+  
+  return (
+    <div>
+      <div className="search-align">
+        <div>
+          <p>Comment Search</p>
+          <input type="search" onChange={this.onchangeComment} onKeyDown={this.keyDownHandler}/>
+        </div>
+        <div>
+          <p>Author search</p>
+          <input type = "search" onChange={this.onChangeAuthor} onKeyDown={this.keyDownHandler}/>
+        </div>     
+      </div>
+      <table className="comment-table">
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Author</th>
+          <th>Likes</th>
+          <th>Replies</th>
+          <th>Comment</th>
+        </tr>
+      </thead>
+      <tbody>
+        {comments.map((comment) => (
+          <tr key={comment.id}>
+            <td>{comment.at}</td>
+            <td>{comment.author}</td>
+            <td>{comment.like}</td>
+            <td>{comment.reply}</td>
+            <td>{comment.text}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    
+    </div>
+    
+  );
+};
+}
+
+export default CommentTable;
